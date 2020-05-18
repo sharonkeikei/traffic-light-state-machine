ba4b6fddd3ab7d0443a87831fabce0f5da7a9dd7
Sharon Cheung <keikei1128@g.ucla.edu>
2020-05-18 15:05:48 -0700

    trafficLight working

--- src/App.js
+++ src/App.js
@@ -39,7 +39,17 @@ const App = () => {
       console.log(state);
       // Your code here to change lightStatus when the 
       //   state of the state machine changes
-
+      switch (state.value) {
+        case 'red':
+          setLightStatus(RED_LIGHT);
+          break;
+        case 'green':
+          setLightStatus(GREEN_LIGHT);
+          break;
+        case 'yellow':
+          setLightStatus(YELLOW_LIGHT);
+          break;
+      }
     });
   }, [])
 
--- src/state-machines/LightMachine.js
+++ src/state-machines/LightMachine.js
@@ -2,10 +2,29 @@ import { createMachine, interpret } from 'xstate';
 
 const lightMachine = createMachine({
   // Your state machine blueprint here
-
+  id: 'lightToggle',
+  initial: 'red',
+  states: {
+    red: { 
+      on: {
+        changeLight: 'green' 
+      }
+    },
+    green: { 
+      on: {
+        changeLight: 'yellow' 
+      }
+    },
+    yellow: {
+      on: {
+        changeLight: 'red' 
+      }
+    },
+  }
 });
 
 
 const service = interpret(lightMachine);
 
 export { lightMachine, service };
+
